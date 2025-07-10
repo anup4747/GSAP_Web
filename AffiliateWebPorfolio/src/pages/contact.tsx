bc4f81c40ce13894353f63e17f08c298b7d4dc2e
anup4747 <anupdtarwade@gmail.com>
2025-07-10 23:11:42 +0530

    added animations in contact page

--- AffiliateWebPorfolio/src/pages/contact.tsx
+++ AffiliateWebPorfolio/src/pages/contact.tsx
@@ -1,20 +1,54 @@
 import type React from "react";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
+import { useRef } from "react";
 
 gsap.registerPlugin(useGSAP);
 
 const Contact: React.FC = () => {
+  const heading = useRef<HTMLHeadingElement | null >(null);
+  const info = useRef<HTMLParagraphElement | null >(null);
   useGSAP(() => {
-    gsap.to(".box", { rotation: "+=360", duration: 3 });
+     gsap.fromTo(
+      heading.current,
+      { opacity: 0, y: 100 },
+      {
+        opacity: 1,
+        y: 0,
+        duration: 1,
+        delay: 0.2,
+        scrollTrigger: {
+          trigger: heading.current,
+          start: "top 80%",
+          toggleActions: "play none none none",
+          once: true,
+        },
+      }
+    );
+    gsap.fromTo(
+      info.current,
+      { opacity: 0, y: 100 },
+      {
+        opacity: 1,
+        y: 0,
+        duration: 1,
+        delay: 0.05,
+        scrollTrigger: {
+          trigger: info.current,
+          start: "top 80%",
+          toggleActions: "play none none none",
+          once: true,
+        },
+      }
+    );
   }, []);
   return (
     <section>
       <div className="flex px-30 py-24 ">
         <div className="w-1/2">
-          <h1 className="leading-tight hero-font text-6xl ">Get in Touch</h1>
+          <h1 ref={heading} className="leading-tight hero-font text-6xl ">Get in Touch</h1>
 
-          <p className="mt-4 paragraph text-lg w-lg">
+          <p ref={info} className="mt-4 paragraph text-lg w-lg">
             We're excited to hear from you. Drop us a message and we'll get back
             to you as soon as possible
           </p>
